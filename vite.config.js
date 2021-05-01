4904743f10878b018ca33921fc8c173ad2af4ade
Prikesh Savla <prikeshsavla@gmail.com>
2021-05-01 10:35:32 +0530

    Change build dir to docs for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-import vue from '@vitejs/plugin-vue'
+import vue from "@vitejs/plugin-vue";
 
 /**
  * https://vitejs.dev/config/
  * @type {import('vite').UserConfig}
  */
 export default {
-  plugins: [vue()]
-}
+  plugins: [vue()],
+  build: { outDir: "docs" },
+};
