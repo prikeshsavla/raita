ad18bfca3b942b0f3b6b0d674db2e314e80dc3ce
Prikesh Savla <34878926+prikeshsavla@users.noreply.github.com>
2024-11-02 06:32:36 +0000

    Change fetch to nocors mode

--- src/components/GithubRepo.vue
+++ src/components/GithubRepo.vue
@@ -166,7 +166,7 @@ export default {
       "https://api.allorigins.win/raw?url=https://api.github.com/repos/"
     );
     // "https://api.allorigins.win/raw?url=https://api.github.com/repos/"
-    this.repository = await fetch(apiURL).then((r) => r.json());
+    this.repository = await fetch(apiURL, { mode: 'no-cors'}).then((r) => r.json());
 
     const ownerURL = `${this.repository.owner.url}`;
     this.owner = await fetch(ownerURL).then((r) => r.json());
